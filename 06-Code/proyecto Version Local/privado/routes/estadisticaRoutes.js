8c4dd508e94864e7717ac8caec81f9d474cd89d8
Panchez Jacome Darwin Javier <96481488+darwinpanchez@users.noreply.github.com>
2025-01-23 20:37:27 -0500

    Delete 06-Code/proyecto Version Local directory

--- 06-Code/proyecto Version Local/privado/routes/estadisticaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const estadisticasController = require('../controllers/estadisticaController'); // Asegúrate de crear este controlador
-
-// Ruta para obtener estadísticas generales
-router.get('/estadisticas', estadisticasController.obtenerEstadisticas);
-
-module.exports = router;
